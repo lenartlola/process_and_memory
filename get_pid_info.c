dfb0e638aea6b4b33c2606559dadea7c1270a87d
lenart <lenartlola@proton.me>
2024-02-29 17:12:17 +0100

    A simple call to print PID

--- get_pid_info.c
+++ get_pid_info.c
@@ -3,11 +3,19 @@
 #include <linux/module.h>
 #include <linux/syscalls.h>
 #include <linux/kernel.h>
+#include <linux/fs_struct.h>
+#include <linux/dcache.h>
+#include <linux/pid.h>
 
-//struct pid_info
+//static char* root_path(struct task_struct *task)
 //{
-//	int	pid;
-//};
+//	struct fs_struct *fs = task->fs;
+//	if (!fs)
+//	{
+//		return NULL;
+//	}
+//	return dentry_path_raw(fs->root.dentry, NULL, 0);
+//}
 
 SYSCALL_DEFINE2(get_pid_info, struct pid_info __user *, info, int, pid)
 {	
@@ -16,7 +24,23 @@ SYSCALL_DEFINE2(get_pid_info, struct pid_info __user *, info, int, pid)
 	
 	pid_struct = find_get_pid(pid);
 	task = pid_task(pid_struct, PIDTYPE_PID);
-	printk(KERN_INFO "pid_info: %s\n", task->comm);
+	if (!task)
+	{
+		printk(KERN_INFO "PID task not found %d\n", pid);
+		return -1;
+	}
+	printk(KERN_INFO "pid_info: pid -> %d\n", task->pid);
+	//char *root = root_path(task);
+	//if (!root) 
+	//{
+	//	printk(KERN_INFO "pid_info: root path not found\n");
+	//	return -1;
+	//}
+	//else
+	//{
+	//	printk(KERN_INFO "pid_info: state -> %d, root path: %s\n", task->pid, root);
+	//	kfree(root);
+	//}
 	return 0;
 }
 
--- test.c
+++ test.c
@@ -3,13 +3,18 @@
 #include <stdio.h>
 #include <string.h>
 
+struct pid_info
+{
+	int	pid;
+};
+
 int main(int argc, char **argv)
 {
-	struct pid_info pid_info;
+	struct pid_info p_info;
 	if (argc < 2)
 		return 1;
 	int pid = atoi(argv[1]);
-	long res = syscall(SYS_get_pid_info, pid);
+	long res = syscall(642, &p_info, pid);
 	printf("RES: %d\n", pid);
 	return res;
 }
