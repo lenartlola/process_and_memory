092174980f22f138a23e2d0004f3e263f3c9c409
lenart <lenartlola@proton.me>
2024-02-29 15:10:45 +0100

    Add linux headers and edit kernel Makefile

--- get_pid_info.c
+++ get_pid_info.c
@@ -1,28 +1,22 @@
 // long sys_get_pid_info(struct pid_info *ret, int pid);
+#include <linux/init.h>
 #include <linux/module.h>
+#include <linux/syscalls.h>
 #include <linux/kernel.h>
 
-# define NO_FLAG 0
-# define NO_FLAG1 1
+//struct pid_info
+//{
+//	int	pid;
+//};
 
-struct pid_info
-{
-	int	pid;
-
-};
-
-SYSCALL_DEFINE2(get_pid_info, struct pid_info __user *, info, int, pid, int, flags)
+SYSCALL_DEFINE2(get_pid_info, struct pid_info __user *, info, int, pid)
 {	
 	struct task_struct	*task;
-	struct pid		*pid_info;
+	struct pid		*pid_struct;
 	
-	if (flags & ~(NO_FLAG | NO_FLAG1))
-		return -EINVAL;
-	if (flag != 0)
-		return -EINVAL;
-	
-	pid_info = find_get_pid(pid);
-	task = pid_task(pid_info, PIDTYPE_PID);
+	pid_struct = find_get_pid(pid);
+	task = pid_task(pid_struct, PIDTYPE_PID);
 	printk(KERN_INFO "pid_info: %s\n", task->comm);
 	return 0;
 }
+
